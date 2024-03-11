0068d340012a49de79ea1a8e16db533adb1d8d76
Corrim Coffman <coffmancorrim@proton.me>
2024-03-11 16:30:41 -0400

    added basic project files

---
+++ app/src/main/java/me/proton/coffmancorrim/acnhvillagercatalog/MainActivity.kt
@@ -0,0 +1,11 @@
+package me.proton.coffmancorrim.acnhvillagercatalog
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
